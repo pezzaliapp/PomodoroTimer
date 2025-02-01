60e99a6a94e726e34526308e689f7c8d22c5ddb2
pezzaliapp <pezzalialessandro@gmail.com>
2025-02-01 15:16:00 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,20 @@
+const CACHE_NAME = 'pomodoro-cache-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon.png',
+  '/icon-large.png',
+  '/start.mp3',
+  '/bell-end.mp3'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('pomodoro-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
@@ -18,6 +23,17 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
     })
   );
 });
